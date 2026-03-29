ad8f0ef23ed8a89283408da07d53563a7ce49b83
aaryan_gulia <aaryan.gulia@gmail.com>
2026-03-29 04:02:13 +0100

    feat: game consts, gamestate and ball structs, and ball movement

--- internal/pong/game.go
+++ internal/pong/game.go
@@ -1,29 +1,48 @@
 package pong
 
-import "fmt"
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"fmt"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
 
 func Hello() {
 	fmt.Println("Hello World")
 }
 
-type Game struct{}
+type Game struct {
+	state GameState
+}
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		fmt.Println("w")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		fmt.Println("s")
+	}
+	g.state.PollState()
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+
+	vector.DrawFilledRect(screen, float32(g.state.ball.x), float32(g.state.ball.y), float32(BallWidth), float32(BallHeight), color.White, false)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 640, 480
 }
 
 func Run() {
+	var state GameState
+	state.Reset()
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(&Game{state: state}); err != nil {
 		fmt.Println("Hello World")
 	}
 }
---
+++ internal/pong/state.go
@@ -0,0 +1,63 @@
+package pong
+
+const (
+	Width                  float64 = 100
+	Height                 float64 = 100
+	PaddleHeight           float64 = 16
+	PaddleWidth            float64 = 2
+	BallHeight             float64 = 2
+	BallWidth              float64 = 2
+	BallVelocityBase       float64 = 1
+	BallVelocityMultiplier float64 = 2
+)
+
+type Ball struct {
+	x  float64
+	y  float64
+	vx float64
+	vy float64
+}
+
+type GameState struct {
+	paddle1 float64
+	paddle2 float64
+	points1 int
+	points2 int
+	ball    Ball
+}
+
+func (g *GameState) PollState() {
+	g.paddleCollision()
+	g.wallCollision()
+	g.moveBall()
+
+}
+
+func (g *GameState) moveBall() {
+	g.ball.x += g.ball.vx
+	g.ball.y += g.ball.vy
+}
+
+func (g *GameState) paddleCollision() {
+	//TODO
+}
+
+func (g *GameState) wallCollision() {
+	if g.ball.y < 0 || g.ball.y > 100 {
+		g.ball.vy *= -1
+	}
+	if g.ball.x < 0 || g.ball.y > 100 {
+		g.Reset()
+	}
+}
+
+func (g *GameState) paddleRebound() {
+	//TODO
+}
+
+func (g *GameState) Reset() {
+	g.ball.x = Width / 2
+	g.ball.y = Height / 2
+	g.ball.vx = BallVelocityBase
+	g.ball.vy = BallVelocityBase
+}
