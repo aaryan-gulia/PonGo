91b40d372b057ad5681b7f1b6769b7c6917d5159
aaryan_gulia <aaryan.gulia@gmail.com>
2026-04-14 00:12:08 +0100

    refactor: seperates game code into play.go from client.go

--- internal/client/client.go
+++ internal/client/client.go
@@ -2,9 +2,6 @@ package client
 
 import (
 	"PonGo/internal/pong"
-	"bytes"
-	"encoding/gob"
-	"image/color"
 	"log"
 	"net"
 
@@ -12,72 +9,21 @@ import (
 )
 
 type Client struct {
-	game *Game
+	game *GameOnlinePVP
 }
 
-func (g *Game) Update() error {
-	e := pong.N
-	if ebiten.IsKeyPressed(ebiten.KeyW) {
-		e = pong.W
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyS) {
-		e = pong.S
-	}
-	handleEvent(e, g.conn)
+func (c *Client) Update() error {
+	c.game.update()
 	return nil
 }
 
-func handleEvent(e pong.GameEvent, conn *net.UDPConn) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	if err := enc.Encode(e); err != nil {
-		log.Fatalln("encoding error : ", err)
-	}
-	_, err := conn.Write(buf.Bytes())
-	if err != nil {
-		log.Println("writing error : ", err)
-	}
-}
-
-func (g *Game) pollState() {
-	buffer := make([]byte, 1024)
-	for {
-		n, _, err := g.conn.ReadFromUDP(buffer)
-		if err != nil {
-			log.Println("read error: ", err)
-			return
-		}
-		var s pong.GameState
-		dec := gob.NewDecoder(bytes.NewReader(buffer[:n]))
-		if err := dec.Decode(&s); err != nil {
-			log.Println("decoding error : ", err)
-			return
-		}
-		g.state = s
-	}
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	h := screen.Bounds().Dy()
-	scale := float64(h) / pong.Height
-
-	drawRect(pong.PaddleWidth*float64(scale), pong.PaddleHeight*float64(scale), 0, g.state.Paddle1*float64(scale), screen)
-	drawRect(pong.PaddleWidth*float64(scale), pong.PaddleHeight*float64(scale), (pong.Width-pong.PaddleWidth)*float64(scale), g.state.Paddle2*float64(scale), screen)
-	drawRect(pong.BallWidth*float64(scale), pong.BallHeight*float64(scale), g.state.Ball.X*float64(scale), g.state.Ball.Y*float64(scale), screen)
-}
-
-func drawRect(w, h, x, y float64, s *ebiten.Image) {
-	newImage := ebiten.NewImage(1, 1)
-	newImage.Fill(color.White)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(w, h)
-	op.GeoM.Translate(x, y)
-	op.ColorScale.ScaleWithColor(color.White)
-	s.DrawImage(newImage, op)
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+func (c *Client) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 640, 480
+}
+
+func (c *Client) Draw(screen *ebiten.Image) {
+	c.game.draw(screen)
+
 }
 
 func Run() {
@@ -97,10 +43,10 @@ func Run() {
 	defer conn.Close()
 	state.Reset()
 
-	game := Game{state: state, conn: conn}
+	game := GameOnlinePVP{Game: Game{state: state}, conn: conn}
 	go game.pollState()
 
-	if err := ebiten.RunGame(&game); err != nil {
+	if err := ebiten.RunGame(&Client{game: &game}); err != nil {
 		log.Fatalln("game engine error :", err)
 	}
 }
--- internal/client/play.go
+++ internal/client/play.go
@@ -2,10 +2,89 @@ package client
 
 import (
 	"PonGo/internal/pong"
+	"bytes"
+	"encoding/gob"
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+	"log"
 	"net"
 )
 
+type GameOnlinePVP struct {
+	Game
+	conn *net.UDPConn
+}
+
+type GameAI struct {
+	Game
+}
+
 type Game struct {
 	state pong.GameState
-	conn  *net.UDPConn
+}
+
+func (g *GameOnlinePVP) update() error {
+	e := pollEvent()
+	g.handleEvent(e)
+	return nil
+}
+
+func pollEvent() pong.GameEvent {
+	e := pong.N
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		e = pong.W
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		e = pong.S
+	}
+	return e
+}
+
+func (g *GameOnlinePVP) handleEvent(e pong.GameEvent) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(e); err != nil {
+		log.Fatalln("encoding error : ", err)
+	}
+	_, err := g.conn.Write(buf.Bytes())
+	if err != nil {
+		log.Println("writing error : ", err)
+	}
+}
+
+func (g *GameOnlinePVP) pollState() {
+	buffer := make([]byte, 1024)
+	for {
+		n, _, err := g.conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Println("read error: ", err)
+			return
+		}
+		var s pong.GameState
+		dec := gob.NewDecoder(bytes.NewReader(buffer[:n]))
+		if err := dec.Decode(&s); err != nil {
+			log.Println("decoding error : ", err)
+			return
+		}
+		g.state = s
+	}
+}
+
+func (g *Game) draw(screen *ebiten.Image) {
+	h := screen.Bounds().Dy()
+	scale := float64(h) / pong.Height
+
+	drawRect(pong.PaddleWidth*float64(scale), pong.PaddleHeight*float64(scale), 0, g.state.Paddle1*float64(scale), screen)
+	drawRect(pong.PaddleWidth*float64(scale), pong.PaddleHeight*float64(scale), (pong.Width-pong.PaddleWidth)*float64(scale), g.state.Paddle2*float64(scale), screen)
+	drawRect(pong.BallWidth*float64(scale), pong.BallHeight*float64(scale), g.state.Ball.X*float64(scale), g.state.Ball.Y*float64(scale), screen)
+}
+
+func drawRect(w, h, x, y float64, s *ebiten.Image) {
+	newImage := ebiten.NewImage(1, 1)
+	newImage.Fill(color.White)
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(w, h)
+	op.GeoM.Translate(x, y)
+	op.ColorScale.ScaleWithColor(color.White)
+	s.DrawImage(newImage, op)
 }
